05abf8f056d2d9326c7e8c68218ce8b04c4e39bb
Beanu <zhaoyunhello@gmail.com>
2013-01-05 23:24:26 +0800

    store data in mongobd

--- apkSpider/pipelines.py
+++ apkSpider/pipelines.py
@@ -2,7 +2,21 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/topics/item-pipeline.html
+import pymongo
+from scrapy.exceptions import DropItem
 
-class ApkspiderPipeline(object):
+class ApkPipeline(object):
+    def __init__(self):
+        connection=pymongo.Connection('localhost',27017)
+        db=connection['test']
+        self.collection=db['scrapy']
+
     def process_item(self, item, spider):
+        vaild=True
+        for data in item:
+            if not data:
+                vaild=False
+                raise DropItem("Missing %s of data from %s" %(data,item['name']))
+        if vaild:
+            self.collection.insert(dict(item))
         return item
--- apkSpider/settings.py
+++ apkSpider/settings.py
@@ -11,5 +11,7 @@ BOT_NAME = 'apkSpider'
 SPIDER_MODULES = ['apkSpider.spiders']
 NEWSPIDER_MODULE = 'apkSpider.spiders'
 
+ITEM_PIPELINES = ['apkSpider.pipelines.ApkPipeline']
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'apkSpider (+http://www.yourdomain.com)'
