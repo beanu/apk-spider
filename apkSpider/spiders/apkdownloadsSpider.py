6093fdd6f068fd22c811fae439e5c486cab3844d
Beanu <zhaoyunhello@gmail.com>
2013-01-11 17:28:16 +0800

    apkdownloads spider

--- apkSpider/spiders/apkdownloadsSpider.py
+++ apkSpider/spiders/apkdownloadsSpider.py
@@ -7,19 +7,23 @@ from apkSpider.items import ApkItem
 class ApkDownloadsSpider(CrawlSpider):
     name="apkdownloads"
     allowed_domains=["apkdownloads.com"]
-    start_urls=["http://www.apkdownloads.com/2013_01_09_archive.html"]
-    rules=[Rule(SgmlLinkExtractor(allow=[r'/2013_\d{2}_\d{2}_archive.html']),follow=True),Rule(SgmlLinkExtractor(allow=[r'game/2013/\d{2}/.+?\.html']),callback='parse_game')]
+    start_urls=["http://www.apkdownloads.com"]
+    #rules=[Rule(SgmlLinkExtractor(allow=[r'/2013_\d{2}_\d{2}_archive.html']),follow=True),Rule(SgmlLinkExtractor(allow=[r'/2013/\d{2}/.+?\.html']),callback='parse_game')]
+    rules=[Rule(SgmlLinkExtractor(allow=[r'/2013_01_10_archive.html']),follow=True),Rule(SgmlLinkExtractor(allow=[r'/2013/\d{2}/.+?\.html']),callback='parse_game')]
 
     def parse_game(self,response):
-        #TODO
         hxs=HtmlXPathSelector(response)
-        values=hxs.select('//div[@id="apkMetaInfo"]/ul')
-        item=ApkItem()
-        item['name']=values.select('li[1]/em/text()').extract()
-        item['packageName']=values.select('li[2]/em/text()').extract()
-        item['version']=values.select('li[3]/em/text()').extract()
-        item['category']=values.select('li[5]/em/text()').extract()
-        item['size']=values.select('li[7]/em/text()').extract()
-        item['lastUpdated']=values.select('li[8]/em/text()').extract()
-        item['tags']=values.select('li[9]/em/a/text()').extract()
-        return item
+        values=hxs.select('//div[@class="post hentry"]/div[2]/div[1]/a/@href').extract()
+        googleUrl=values[0]
+        downloadUrl=values[1]
+        yield Request(url=googleUrl,callback=self.parse_google)
+
+    def parse_google(self,response):
+        hxs=HtmlXPathSelector(response)
+        #title
+        div_title=hxs.select('//div[@class="doc-banner-container"]')
+        apkname=div_title.select('h1[@class="doc-banner-title"]/text()').extract()
+        company=div_title.select('a[@class="doc-header-link"]/text()').extract()
+        apkicon=div_title.select('div[@class="doc-banner-icon"]/img/@src').extract()
+        price=div_title.select('span[@class="buy-button-price"]/text()').extract()
+
